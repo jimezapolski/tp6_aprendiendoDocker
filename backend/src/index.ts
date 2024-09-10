5c8508134b0ac49223d631b91a2b932b1ce1f6a8
jimezapolski <jimezapolski@gmail.com>
2024-09-09 22:47:24 -0300

    intentandolo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, NextFunction} from "express";
+import express,{ Express, Request, Response, NextFunction} from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { agregarImagen, agregarPerfil, avisoPosteo, borrarPerfil, listarImagenesPorPerfil, listarPerfiles} from "./Modelo";
