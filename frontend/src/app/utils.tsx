a645b32b2dcad57938c8488039807d79431d3930
jimezapolski <jimezapolski@gmail.com>
2024-09-11 18:11:23 -0300

    intento mil

--- frontend/src/app/utils.tsx
+++ frontend/src/app/utils.tsx
@@ -8,7 +8,6 @@ import {
 
 export async function api<T>(url: string): Promise<T> {
   const urlCompleta = `${process.env.NEXT_PUBLIC_URL_API}${url}`;
-  console.log(urlCompleta);
   const response = await fetch(urlCompleta);
   if (!response.ok) {
     throw new Error(response.statusText);
